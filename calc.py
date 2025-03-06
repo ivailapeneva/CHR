ba69e0881f2a294123220fb4aed3f36db6865734
ivailapeneva <ivailapeneva@gmail.com>
2025-03-06 11:15:05 +0200

    calculated add

---
+++ calc.py
@@ -0,0 +1,13 @@
+x=int(input("x="))
+print()
+y=int(input("y="))
+print()
+operation =input("operation: ")
+
+if operation =='+':
+    print(x+y)
+elif operation =='-':
+    print(x-y)
+elif operation == '*':
+    print(x*y)
+    
